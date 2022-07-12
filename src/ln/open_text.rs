62288ea334b4ee9ccd3d8b038ee9899821a2b203
newbee1905 <beenewminh@outlook.com>
2022-07-12 12:55:35 +0700

    (feat) Soft wrap text with fold

--- src/ln/open_text.rs
+++ src/ln/open_text.rs
@@ -1,11 +1,20 @@
 use std::io::Result;
-use std::process::{Command, ExitStatus};
+use std::process::{Command, ExitStatus, Stdio};
 
 pub fn open_bat() -> Result<ExitStatus> {
+    let soft_wrap = match Command::new("fold")
+        .arg("-s")
+        .arg("/tmp/log_e")
+        .stdout(Stdio::piped())
+        .spawn()
+    {
+        Err(why) => panic!("couldn't spawn wc: {}", why),
+        Ok(soft_wrap) => soft_wrap,
+    };
     Command::new("bat")
         .arg("--paging")
         .arg("always")
-        .arg("/tmp/log_e")
+        .stdin(soft_wrap.stdout.unwrap())
         .spawn()?
         .wait()
 }
